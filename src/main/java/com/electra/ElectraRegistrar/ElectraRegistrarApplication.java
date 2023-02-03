c86597efdf8449e395fb43686a8c833d17eda1ac
David Adenuga <adenugadavid8@gmail.com>
2023-02-03 17:12:56 +0100

    First push

---
+++ src/main/java/com/electra/ElectraRegistrar/ElectraRegistrarApplication.java
@@ -0,0 +1,13 @@
+package com.electra.ElectraRegistrar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectraRegistrarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectraRegistrarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/electra/ElectraRegistrar/ElectraRegistrarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electra.ElectraRegistrar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectraRegistrarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
