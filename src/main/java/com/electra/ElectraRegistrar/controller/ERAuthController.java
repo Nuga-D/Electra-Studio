e5fb41339a1a1b09df706fb8fe0404ca747fae30
David Adenuga <adenugadavid8@gmail.com>
2023-05-28 21:10:23 +0100

    added the frontend urls

--- src/main/java/com/electra/ElectraRegistrar/controller/ERAuthController.java
+++ src/main/java/com/electra/ElectraRegistrar/controller/ERAuthController.java
@@ -47,7 +47,7 @@ public class ERAuthController {
     @Autowired
     private CompanyService companyService;
 
-    @CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST})
+    @CrossOrigin(origins = "https://etaxpayer-syntechsysworld.vercel.app/session/signin", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST})
     @PostMapping("/signin")
     public ResponseEntity<?> login(@RequestBody LoginRequest loginForm) {
         String username = loginForm.getUsername();
@@ -61,7 +61,7 @@ public class ERAuthController {
     }
 
 
-    @CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST})
+    @CrossOrigin(origins = "https://etaxpayer-syntechsysworld.vercel.app/session/signup", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST})
     @PostMapping("/signup")
     public ResponseEntity registerUser(@RequestBody SignupRequest signupRequest) {
 
